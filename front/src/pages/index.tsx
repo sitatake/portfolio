51f267d0de1830f6be77af0d78802989084ee821
takeshita <tkstdic@gmail.com>
2022-02-27 23:36:55 +0900

    create environment

---
+++ front/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Center, Square, Circle } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <>
+      <Center bg="tomato" h="100px" color="white" fontSize="30">
+        Portfolio
+      </Center>
+      <p>test</p>
+    </>
+  );
+}
